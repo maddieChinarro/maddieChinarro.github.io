53d67ba50ee38d5ca7b834781f73cd435a62a172
Madeline Chinarro <madeline.chinarro@outlook.com>
2025-04-21 13:00:15 +0200

    tweaked map
    added 4k version of the map for PCs, changed map css, added 48x48 favico

--- ressources/scripts/map.js
+++ ressources/scripts/map.js
@@ -1,19 +1,32 @@
 const map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: -2,
+    minZoom: -1.5,
     maxZoom: 2,
     zoomSnap: 0,
     attributionControl: false
 });
 
+let screenType = window.matchMedia("(max-width: 700px)")
+
 const bounds = [[0,0], [1536,2048]];
-const image = L.imageOverlay('/ressources/img/mapBackground_2048.png', bounds).addTo(map);
+
+if (screenType.matches) {
+    const image = L.imageOverlay('/ressources/img/mapBackground_2048.webp', bounds).addTo(map);
+} else {
+    const image = L.imageOverlay('/ressources/img/mapBackground_4096.webp', bounds).addTo(map);
+}
 
 const valimar = L.marker([540, 800]).addTo(map);
 valimar.bindPopup("<a href='/villes/valimar/'>Valimar</a>").openPopup();
-const thaldrassin = L.marker([525, 1852]).addTo(map);
+const thaldrassin = L.marker([528, 1846]).addTo(map);
 thaldrassin.bindPopup("<a href='/villes/thaldrassin/'>Thaldrassin</a>").openPopup();
 const eauclaire = L.marker([625, 1696]).addTo(map);
 eauclaire.bindPopup("<a href='/villes/eauclaire/'>Eauclaire</a>").openPopup();
 
 map.fitBounds(bounds);
+
+if (screenType.matches) {
+    map.setZoom(-1);
+} else {
+    map.setZoom(0);
+}
